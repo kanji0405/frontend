aa52cd91f8612062711f74f9254eddf1485b2f0e
kanji0405 <12500577s@gmail.com>
2022-05-13 08:54:54 +0900

    add PrefWrapper

--- src/App.js
+++ src/App.js
@@ -1,68 +1,95 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { LineChart, Line, Data, XAxis, Tooltip, CartesianGrid } from 'recharts';
-let xApiKey = require('./X_API_KEY');
+import xApiKey from './X_API_KEY.json';
 
 async function getAPI(name) {
 	return new Promise((resolve, reject) => {
 		try {
 			fetch('https://opendata.resas-portal.go.jp/' + name, {
-				headers: { 'X-API-KEY': xApiKey },
-			}).then((respond) => {
-				resolve(respond.json());
+				headers: { 'X-API-KEY': xApiKey.key },
+			}).then((res) => {
+				if (res.status == 200){
+					resolve(res.json());
+				}else{
+					throw '接続失敗';
+				}
 			});
 		} catch (e) {
 			reject(null);
+			throw e;
 		}
 	});
 }
-console.log(xApiKey)
+
 /*
-getAPI('api/v1/prefectures').then((json) => {
-	console.log(json.result);
-	//json.result.map(pref => { return <PrefButton>{pref}</PrefButton>; })
+getAPI('api/v1/population/composition/perYear?cityCode=11362&prefCode=11').then((json) => {
+	console.log(json);
 });
 */
 
-class PrefButton extends React.Component {
-	get _knsIsSelected() { return this.state.value; }
-	constructor(props) {
+export default class App extends React.Component{
+	static value;
+	constructor(props){
 		super(props);
-		this.state = { value: '' };
-		this._knsItem = this.props.children;
-		this._knsId = "pref" + this._knsItem.prefCode;
+		this.prefs = [];
+		this.state = { dirty: false };
+		this._changeSelection = this._onChange.bind(this);
 	}
-	render() {
+	componentDidMount(){
+		if (this.prefs.length > 0){
+			return;
+		}
+		getAPI('api/v1/prefectures').then(async json => {
+			this.prefs = json.result.map(pref => new PrefWrapper(pref));
+			this.setState({dirty: true});
+		});
+	}
+	_onChange(){
+		this.setState({dirty: false});
+		console.log('changed', this.prefs.length);
+	}
+	render(){
 		return (
-			<form>
-				<label>
-					<input type="checkbox" name="pref"
-						onChange={this.handleChange.bind(this)}
-						value={this._knsId}
-					/>{this._knsItem.prefName}
-				</label>
-			</form>
+			<div>
+				{ this.prefs.map(pref => pref.toStr(this._changeSelection)) }
+				<LineChart
+					width={400}
+					height={400}
+					data={200}
+					margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+				<XAxis dataKey="name" />
+				<Tooltip />
+				<CartesianGrid stroke="#f5f5f5" />
+				<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+				<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+				</LineChart>
+			</div>
 		);
 	}
-	handleChange(event) {
-		this.setState({ value: event.target.value });
+}
+
+class PrefWrapper{
+	constructor(item){
+		this.item = item;
+		this._isSelected = false;
+	}
+	toStr(onchange){
+		return (
+			<label key={this.id}>
+				<input type="checkbox" name="prefs"
+					onChange={onchange} value={id}
+				/>{this.name}
+			</label>
+		);
+	}
+	get isSelected(){
+		return this._isSelected;
+	}
+	get id(){
+		return "key" + this.item.prefCode;
+	}
+	get name(){
+		return this.item.prefName;
 	}
 }
-
-function App(){
-	return (
-		(<LineChart
-			width={400}
-			height={400}
-			data={200}
-			margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-		>
-			<XAxis dataKey="name" />
-			<Tooltip />
-			<CartesianGrid stroke="#f5f5f5" />
-			<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-			<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-		</LineChart>)
-	);
-}
-
-export default App;
