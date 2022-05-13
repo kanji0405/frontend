47c43ae56411508385778d6ea7b6ad9e9e5539a3
kanji0405 <12500577s@gmail.com>
2022-05-13 19:38:53 +0900

    system works correctly, made css

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Strings from './Strings.json';
 //====================================================================================
 // - RESAS APIを叩く静的クラスです
 //====================================================================================
-class APIManager{
+class APIManager {
 	static baseUrl = 'https://opendata.resas-portal.go.jp/api/';
-	static parseUrl(name){ return this.baseUrl + name; }
+	static parseUrl(name) { return this.baseUrl + name; }
 	static getAPI(name) {
 		return new Promise((resolve, reject) => {
 			try {
 				fetch(this.parseUrl(name), { headers: { 'X-API-KEY': xApiKey.key }, }
 				).then((res) => {
-					if (res.status === 200){
+					if (res.status === 200) {
 						resolve(res.json());
-					}else{
+					} else {
 						throw Error(Strings.ApiConnectionFailed);
 					}
 				});
@@ -31,55 +31,56 @@ class APIManager{
 }
 
 //====================================================================================
-// PrefWrapper
+// PrefController
 //====================================================================================
 // - 都道府県ごとの選択状況、人口データの取得、レンダリング用のオブジェクトを
-//   生成するラッパークラスです
+//   生成するコントローラクラスです
 //====================================================================================
-class PrefWrapper{
+class PrefController {
 	static ColorList = ['red', 'blue', 'green', 'purple', 'orange', 'gray'];
-	get id(){ return "key" + this.item.prefCode; }
-	get name(){ return this.item.prefName; }
-	get isChecked(){
+	get id() { return "key" + this.item.prefCode; }
+	get name() { return this.item.prefName; }
+	get isChecked() {
 		const el = document.querySelector(`input[name='prefs'][value=${this.id}]`);
 		return el && el.checked === true;
 	}
-	constructor(parent, item){
+	constructor(parent, item) {
 		this.parent = parent;
 		this.item = item;
 		this.population = null;
 	}
-	getPopulation(){
-		if (this.population === null){
+	getPopulation() {
+		if (this.population === null) {
 			APIManager.getAPI(
 				`v1/population/composition/perYear?cityCode=-&prefCode=${this.item.prefCode}`
 			).then(
 				(popl) => {
 					this.population = popl.result;
-					this.requestChange();
+					this.requestUpdate();
 				}
 			);
 		}
 		return this.population;
 	}
-	requestChange(){
+	requestUpdate() {
 		this.parent._changeSelection();
 	}
-	renderCheckbox(){
+	renderCheckbox() {
 		return (
-			<label key={this.id + '_label'} draggable="false">
-				<input type="checkbox" name="prefs" onChange={this.requestChange.bind(this)} value={this.id}/>
-				{this.name}
+			<label key={this.id + '_label'}>
+				<input type="checkbox" name="prefs" onChange={this.requestUpdate.bind(this)} value={this.id} />
+				<span draggable="false">{this.name}</span>
 			</label>
 		);
 	}
-	renderLine(i){
+	renderLine(i) {
+		const colors = PrefController.ColorList;
 		return (
 			<Line
 				key={this.id + '_line'}
 				dataKey={this.id}
 				name={this.name}
-				stroke={PrefWrapper.ColorList[i % PrefWrapper.ColorList.length]}
+				stroke={colors[i % colors.length]}
 				type="monotone"
 			/>
 		);
@@ -87,86 +88,104 @@ class PrefWrapper{
 }
 
 //====================================================================================
-// PopulationModel
+// GraphModel
 //====================================================================================
-// - ステートの管理、グラフデータのフォーマット、レンダリングを行う
-//   総合的なモデルクラスです
+// - グラフデータのフォーマット、レンダリング用のオブジェクトを
+//   生成するモデルクラスです
 //====================================================================================
-export default class PopulationModel extends React.Component{
-	static value;
-	constructor(props){
-		super(props);
-		this.state = {
-			prefWrappers: [],
-			graphData: [],
-			selectedPrefs: []
-		};
-		this._changeSelection = this._onChange.bind(this);
+class GraphModel {
+	constructor() {
+		this.graphData = [];
+		this.selectedPrefs = [];
 	}
-	componentDidMount(){
-		if (this.state.prefWrappers.length > 0){
-			return;
-		}
-		APIManager.getAPI('v1/prefectures').then(json => {
-			this.setState({
-				prefWrappers: json.result.map(pref => new PrefWrapper(this, pref))
-			});
-		});
-	}
-	_onChange(){
-		this.formatGraphData();
-	}
-	formatGraphData(){
-		const newGraph  = [];
-		const newSelect = [];
-		this.state.prefWrappers.forEach(pref=>{
-			if (pref.isChecked === false){
-				return;
-			}
+	formatData(prefs) {
+		this.graphData = [];
+		this.selectedPrefs = [];
+		prefs.forEach(pref => {
+			if (pref.isChecked === false) { return; }
 			const popl = pref.getPopulation();
-			if (popl === null){
-				return;
-			}
-			newSelect.push(pref);
+			if (popl === null) { return; }
+			this.selectedPrefs.push(pref);
 			popl.data[0].data.forEach((json) => {
 				const year = json.year;
-				let yearObject = newGraph.find(obj => obj.year === year);
-				if (yearObject === undefined){
-					yearObject = {year: year};
-					newGraph.push(yearObject);
+				let yearObject = this.graphData.find(obj => obj.year === year);
+				if (yearObject === undefined) {
+					yearObject = { year: year };
+					this.graphData.push(yearObject);
 				}
 				yearObject[pref.id] = json.value;
 			});
 		});
-		newGraph.sort((a, b) => a.year - b.year);
-		this.setState({graphData: newGraph, selectedPrefs: newSelect});
+		return this.graphData.sort((a, b) => a.year - b.year);
 	}
-	render(){
+	renderGraph() {
 		return (
-			<div>
-				<h2>{Strings.Title}</h2>
-				{ this.state.prefWrappers.map(pref => pref.renderCheckbox()) }
-				<ResponsiveContainer width="100%" height={500}>
-					<LineChart
-						width='800' height='400' data={this.state.graphData}
-						margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-					>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis 
+			<ResponsiveContainer width="90%" height={400} >
+				<LineChart
+					width='800' height='400' data={this.graphData}
+					margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+					<CartesianGrid strokeDasharray="5 5" />
+					<XAxis
 						dataKey="year"
-						domain={['dataMin', 'dataMax']}
+						domain={[1970, 2020]}
+						allowDataOverflow={true}
+						type="number"
 						label={{ value: Strings.LabelYear, offset: -5, position: "insideBottomRight" }}
 					/>
 					<YAxis
-						domain={['dataMin', 'dataMax']}
+						domain={[0, 8000000]}
+						type="number"
 						label={{ value: Strings.LabelPopulation, angle: -90, position: "insideLeft" }}
 					/>
 					<Tooltip />
 					<Legend verticalAlign="top" />
-					{ this.state.selectedPrefs.map((pref, i)=> pref.renderLine(i)) }
-					</LineChart>
-				</ResponsiveContainer>
-			</div>
+					{this.selectedPrefs.map((pref, i) => pref.renderLine(i))}
+				</LineChart>
+			</ResponsiveContainer>
+		);
+	}
+}
+
+//====================================================================================
+// MainView
+//====================================================================================
+// - 主画面のレンダリングを行うビュークラスです
+//====================================================================================
+export default class MainView extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { isDirty: false };
+		this._prefControllers = [];
+		this._graphModel = new GraphModel();
+		this._changeSelection = this._onChange.bind(this);
+	}
+	componentDidMount() {
+		if (this._prefControllers.length > 0) { return; }
+		APIManager.getAPI('v1/prefectures').then(json => {
+			this._prefControllers = json.result.map(
+				item => new PrefController(this, item)
+			);
+			this.setState({ isDirty: true });
+		});
+	}
+	_onChange() {
+		this._graphModel.formatData(this._prefControllers);
+		this.setState({ isDirty: true });
+	}
+	render() {
+		return (
+			<article className="poplModelArticle">
+				<h1>{Strings.Header1}</h1>
+				<div className="poplModelContents">
+					<h2>{Strings.Header2}</h2>
+					<div className="poplModelPrefs">
+						{this._prefControllers.map(pref => pref.renderCheckbox())}
+					</div>
+					<hr />
+					{this._graphModel.renderGraph()}
+				</div>
+			</article>
 		);
 	}
 }
